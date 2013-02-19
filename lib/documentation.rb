96e2b4fb52a14b3f5528047f6fa73e49f9fc90d9
Ian MacLeod <ian@nevir.net>
2013-02-18 18:59:45 -0800

    Let's not get ahead of ourselves.

--- lib/documentation.rb
+++ lib/documentation.rb
@@ -1,14 +1,8 @@
+require "forwardable"
 require "documentation/context"
 
 module Documentation
-
   DOC_ROOT = File.expand_path("../../documentation", __FILE__)
-
-  # See [`Documentation::Context`](documentation/context.rb) for a full
-  # reference of context methods.
-  def self.default_context
-    @default_context ||= context_for(:en)
-  end
 
   def self.context_for(lang, options={})
     Documentation::Context.new(DOC_ROOT, lang, options)
